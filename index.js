5a7487b453d16531f013bcf0b02ace98423c44cf
Manuel Spigolon <behemoth89@gmail.com>
2023-01-24 23:31:31 +0100

    draft: POC (#1)
    * wip
    
    * wip: github action

---
+++ index.js
@@ -0,0 +1,22 @@
+
+import pino from 'pino'
+import * as core from '@actions/core'
+// import * as github from '@actions/github'
+
+import { upsertStatusBoard } from './statusboard.js'
+
+try {
+  const logger = pino({
+    transport: {
+      target: 'pino-pretty'
+    }
+  })
+
+  const notionToken = core.getInput('notion-token')
+  const databaseId = core.getInput('database-id')
+
+  // github.context.payload
+  await upsertStatusBoard({ logger, notionToken, databaseId })
+} catch (error) {
+  core.setFailed(error.message)
+}
